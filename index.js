ee077acff099de6a1a3b9855c31798eb0ccd6e3f
Joao <joaoeric35@gmail.com>
2024-08-02 16:14:07 -0300

    teste

--- index.js
+++ index.js
@@ -9,9 +9,11 @@ servidor.get('/calculadora/somar/:n1/:n2', (req, resp) =>   {
     let soma = n1 + n2
 
     resp.send ('a soma é ' + soma)
+ 
 
+    
 }  )
 
 servidor.listen(5050, () => console.log('API subiu na porta 5050') );
 
-console.log('vai se fuder');
+console.log('vai se fuderaa');
